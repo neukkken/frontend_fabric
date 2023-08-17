7c0cf6f2b2b52e6361ae2c7e98c9469890fa015d
robinsontru <robinsontruque04@gmail.com>
2023-08-17 12:06:23 -0500

    bucles desde la fabric

---
+++ Fabric/Bucles/main.js
@@ -0,0 +1,110 @@
+//sintaxis del switch
+
+// let num = "texto";
+
+// switch (num){
+//     case "texto": console.log("que");
+//     break;
+
+//     case 2: console.log("que2");
+//     break;
+
+//     default: (alert("HOLA"));
+//     break;
+// }
+
+//EJERCICIO 1 SWITCH
+
+// let productoABuscar = parseInt(prompt("Digite el producto que desea ver el precio 1. celular 2. camisa 3. auto"));
+// let arrayProductos = [300, 40, 50000,];
+
+// console.log(productoABuscar);
+
+// switch(productoABuscar){
+//     case 1: alert(arrayProductos[0]);
+//         break;
+
+//     case 2: alert(arrayProductos[1]);
+//         break;
+
+//     case 3: alert(arrayProductos[2]);
+//         break;
+
+//     default:
+//         alert ("No hay nada")
+// }
+
+
+// sintexis del while 
+
+// let contador = 0;
+// let a = 2;
+
+// while (contador <= 10){
+    
+//     a=a*a;
+
+//     console.log(a);
+
+//  contador++;
+// }
+
+//EJERCICIO 2 WHILE
+
+// let contNum = 0;
+
+
+// while (contNum < 5){
+
+//     let numDigitado = prompt("Digite un numero pa");
+    
+//     if(numDigitado%3==0 && numDigitado%5==0){
+//         console.log(`El numero ${numDigitado} es ping pong`);
+//     }else if(numDigitado%3==0){
+//         console.log(`El numero ${numDigitado} es ping`);
+//     }else if(numDigitado%5==0){
+//         console.log(`El numero ${numDigitado} es pong`);
+//     }else{
+//         console.log(numDigitado);
+//     }
+
+
+//     contNum++;
+// }
+
+// sintaxis del FOR
+
+// let num = prompt("Digite un numero");
+
+// for (let i = 1; i <= num; i++) {
+
+//     console.log(i+1);
+
+// }
+
+
+//EJERCICIO 3 FOR
+
+// let numIntroducido = parseInt(prompt("Introduzca un numero"));
+
+// for (let i = 1; i <= numIntroducido; i++) {
+//     if (i%2 == 0) {
+//         console.log(`${i} es un numero par`);
+//     }else if(i%2 == 1) {
+//         console.log(`${i} es un numero impar`);
+//     }
+
+// }
+
+
+
+// EJERCICIO 4 FOR OTRA VE
+
+// let tamañoDelTriangulo = parseInt(prompt("Digite el tamaño del triangulo"));
+// let asterisco = "";
+
+// for (let i = 0; i < tamañoDelTriangulo; i++) {
+
+//     console.log(asterisco=asterisco+" * ");
+
+// }
